5c1bdb0d44f3a6922ba8d6f8402f2290738cb991
damiano <damianomails@gmail.com>
2021-01-20 22:23:18 +0100

    renamed project

--- android/app/src/main/java/com/cinepin/MainActivity.java
+++ android/app/src/main/java/com/cinepin/MainActivity.java
@@ -1,4 +1,4 @@
-package com.appts;
+package com.cinepin;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "appts";
+    return "cinepin";
   }
 }
