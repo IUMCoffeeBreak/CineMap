b59c78771355e7382ce9f14b528f279e16bb25f9
damiano <damianomails@gmail.com>
2021-01-24 19:54:11 +0100

    working map examples

--- src/components/navbar-tabs/Homepage.tsx
+++ src/components/navbar-tabs/Homepage.tsx
@@ -1,10 +1,41 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, StyleSheet, Text } from "react-native";
+import MapView, { UrlTile } from "react-native-maps";
+
+const styles = StyleSheet.create({
+    map: {
+        ...StyleSheet.absoluteFillObject
+    }
+});
 
 export function Homepage() {
     return (
         <SafeAreaView style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
             <Text style={{ alignItems: "center" }}>Homepage</Text>
+            {/* NAtive map example */}
+            {/*<MapView*/}
+            {/*  */}
+            {/*    style={styles.map}*/}
+            {/*    initialRegion={{*/}
+            {/*        latitude: 41.9028,*/}
+            {/*        longitude: 12.4964,*/}
+            {/*        latitudeDelta: 0.5,*/}
+            {/*        longitudeDelta: 0.5*/}
+            {/*    }}*/}
+            {/*/>*/}
+
+            {/* OSM Example*/}
+            <MapView
+                style={styles.map}
+                initialRegion={{
+                    latitude: 41.9028,
+                    longitude: 12.4964,
+                    latitudeDelta: 0.5,
+                    longitudeDelta: 0.5
+                }}
+            >
+                <UrlTile urlTemplate={"http://c.tile.openstreetmap.org/{z}/{x}/{y}.png"} maximumZ={19} flipY={false} />
+            </MapView>
         </SafeAreaView>
     );
 }
