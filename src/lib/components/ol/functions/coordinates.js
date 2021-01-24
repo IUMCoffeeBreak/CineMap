7b5003e911ca5c79c822ed634aa7203142ca686f
damiano <damianomails@gmail.com>
2021-01-24 22:14:13 +0100

    removed unused ol component

--- src/lib/components/ol/functions/coordinates.js
+++
@@ -1,7 +0,0 @@
-import { transform } from "ol/proj";
-
-export const swap = ([a, b]) => [b, a];
-
-export const convertPointsToEPSG3857 = points => points.map(convertPointToEPSG3857);
-
-export const convertPointToEPSG3857 = point => transform(swap(point), "EPSG:4326", "EPSG:3857");
