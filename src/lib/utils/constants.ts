88feb0e363a80537f34469a60dbfc1486bb3656b
damiano <damianomails@gmail.com>
2021-01-26 15:07:37 +0100

    new constants

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -2,7 +2,30 @@ export const constants = {
     tabs: {
         HOME: "Home",
         SETTINGS: "Settings",
-        MAP: "Map"
+        MAP: "Map",
+        SEARCH: "Search"
+    },
+    colors: {
+        GLOBAL_BACKGROUND_COLOR: "#eeeeee",
+        TRANSPARENT: "rgba(0, 0, 0, 0)"
+    },
+    text: {
+        TITLE_FONT: 18
+    },
+    cards: {
+        RADIUS: 10,
+        BACKGROUND_COLOR: "white",
+        MARGIN_BOTTOM: 10,
+        MARGIN_TOP: 10,
+    },
+    spacing: {
+        MARGIN_BOTTOM: 20,
+        MARGIN_TOP: 20,
+        MARGIN_LEFT: 20,
+        MARGIN_RIGHT: 20,
+    },
+    borders: {
+        RADIUS: 10,
     }
 };
 
