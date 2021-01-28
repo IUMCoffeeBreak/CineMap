94a44779b12e9575aaab4142e5821cc65ce20c1c
damiano <damianomails@gmail.com>
2021-01-28 01:51:17 +0100

    geolocation function

---
+++ src/lib/geolocation.ts
@@ -0,0 +1,44 @@
+import { buildQs } from "./utils/functions";
+
+export interface SearchProps {
+    street?: string;
+    city?: string;
+    county?: string;
+    state?: string;
+    country?: string;
+    format?: "xml" | "json" | "jsonv2" | "geojson" | "geocodejson";
+    postalcode?: string;
+}
+
+export interface Geolocation {
+    place_id: number;
+    licence: string;
+    osm_type: string;
+    osm_id: number;
+    boundingbox: number[];
+    lat: number;
+    lon: number;
+    display_name: string;
+    class: string;
+    type: string;
+    importance: number;
+    icon: string;
+}
+
+export async function searchLocation(props: SearchProps): Promise<Geolocation[] | null> {
+    props.format = props.format || "json";
+    const url = `https://nominatim.openstreetmap.org/search?${buildQs(props)}`;
+    try {
+        const response = await fetch(url);
+        const json: Geolocation[] = await response.json();
+        return json.map(geo => {
+            geo.boundingbox = geo.boundingbox.map(Number);
+            geo.lat = Number(geo.lat);
+            geo.lon = Number(geo.lon);
+            return geo;
+        });
+    } catch (e) {
+        console.error("search location error:", e);
+        return null;
+    }
+}
