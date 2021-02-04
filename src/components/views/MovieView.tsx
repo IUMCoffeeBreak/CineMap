a8512feffcc0a7e9fb93a2620ed0d342261a38cc
gabriele fraracci <gbrl.frc@protonmail.ch>
2021-02-04 01:47:44 +0000

    took care of moview view styling

--- src/components/views/MovieView.tsx
+++ src/components/views/MovieView.tsx
@@ -1,24 +1,14 @@
-import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Image, ImageBackground, StyleSheet, Text, View, Animated } from "react-native";
 import MapView from "react-native-maps";
-import { Cast, LocationPin, Preview } from "../../lib/utils/types";
 import { SafeAreaView } from "../../lib/components/SafeAreaView";
-
-export interface MovieScreenProps {
-    title: string;
-    descripion?: string;
-    locationsPins?: LocationPin[];
-    director: string;
-    cast?: Cast[];
-    preview: Preview;
-}
 
 export const romeCoordinates = {
     lat: 41.9028,
     lon: 12.4964
 };
 
-export function MovieView({props: MovieScreenProps, route, navigation}) {
+export function MovieView({route, navigation}) {
 
   const {filmTitle, filmActor, filmDirector, filmPlot, filmPoster,
         filmRating, filmYear}=route.params;
@@ -27,80 +17,129 @@ export function MovieView({props: MovieScreenProps, route, navigation}) {
           <SafeAreaView style={movieViewStyles.mainContainer}>
               <View style={movieViewStyles.headerContainer}>
                   <View style={movieViewStyles.headerInfoContainer}>
-                      <Text style={movieViewStyles.title}>
-                          {filmTitle}
-                      </Text>
+                        <Text style={movieViewStyles.title}>
+                            {filmTitle}
+                        </Text>
                       <Text style={movieViewStyles.headerInfo}>
-                          {filmYear}{"\n"}
-                          Regista: {filmDirector}
-                          {"\n"}Cast: {filmActor}
+                          {filmYear} - {filmRating} / 10 {'\n'}
+                          Regista: {filmDirector}{'\n'}
+                          Cast: {filmActor}
                       </Text>
                   </View>
-                  <Image
-                      source={{uri: filmPoster}}
-                      resizeMode="contain"
-                      style={movieViewStyles.image}
-                  />
-              </View>
-              <MapView
-                  style={movieViewStyles.mapView}
-                  initialRegion={{
-                      latitude: romeCoordinates.lat,
-                      longitude: romeCoordinates.lon,
-                      latitudeDelta: 0.5,
-                      longitudeDelta: 0.5
-                  }}
-              />
-              <Text style={movieViewStyles.cheatSheet}>
-                Rating: {filmRating}{"\n\n"}  
-                {filmPlot}
-              </Text>
-          </SafeAreaView>
+                    <ImageBackground
+                        style={movieViewStyles.image}
+                        source={{uri: filmPoster}}
+                        resizeMode='contain'
+                    />
+                </View>
+
+                
+            <MapView
+                style={movieViewStyles.mapView}
+                initialRegion={{
+                    latitude: romeCoordinates.lat,
+                    longitude: romeCoordinates.lon,
+                    latitudeDelta: 0.5,
+                    longitudeDelta: 0.5
+                }}
+            />
+            <View style={movieViewStyles.cheatSheetContainer}>
+                <Text style={movieViewStyles.cheatSheet}> 
+                    {filmPlot}
+                </Text>
+            </View>
+        </SafeAreaView>
       </>
   );
 }
 
 const movieViewStyles = StyleSheet.create({
     mainContainer: {
-        flex: 1
+        flex: 1,
+
     },
     headerContainer: {
         flex: 1,
         flexDirection: "row",
-        marginTop: 15
+        marginTop: 15,
+        backgroundColor: 'white',
+        borderRadius: 5,
+        margin: 15,
+        shadowOffset: {
+            height: 0,
+            width: 1
+          },
+        elevation: 5,
+        shadowOpacity: 0.22,
+        shadowRadius: 2.22,
     },
     headerInfoContainer: {
-        flex: 1
+        flex: 1,
+
     },
     title: {
-        color: "#121212",
-        width: 188,
-        height: 100,
-        textAlign: "center"
+        color: 'black',
+        fontSize: 20,
+        maxWidth: '80%',
+        fontWeight: 'bold',
+        fontStyle: 'italic',
+        margin: 15
     },
+
+
     headerInfo: {
-        color: "#121212",
+        color: 'black',
         height: 100,
         width: 188,
-        textAlign: "center"
+        margin: 15,
+        marginBottom: 0,
+        fontStyle: 'italic'
     },
 
     image: {
-        width: 188,
-        height: 200
+        width: 138,
+        height: 200,
+        marginTop: 1,
     },
 
     mapView: {
         width: "93%",
-        height: 229,
+        height: 230,
+        borderRadius: 10,
         backgroundColor: "#E6E6E6",
-        marginLeft: 15
+        marginLeft: 15,
+        shadowOffset: {
+            height: 0,
+            width: 1
+          },
+        elevation: 5,
+        shadowOpacity: 0.22,
+        shadowRadius: 2.22,
+
+    },
+
+    cheatSheetContainer: {
+        backgroundColor: 'white',
+        width: "93%",
+        shadowOffset: {
+            height: 0,
+            width: 1
+          },
+        elevation: 5,
+        shadowOpacity: 0.22,
+        shadowRadius: 2.22,
+        margin: 15,
+        marginBottom: 135,
+        borderRadius: 5
     },
     cheatSheet: {
-        color: "#121212",
-        height: 229,
-        width: "95%",
-        marginTop: 20,
-        marginLeft: 15
+        fontStyle: 'italic',
+        fontSize: 15,
+        color: "black",
+        textAlign: 'center',
+        margin: 15
+
+        
+        
     }
 });
