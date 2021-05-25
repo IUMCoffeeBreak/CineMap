a0d1554092259259d6fbd11d9396e66c44ea4cf9
dadna <dd@no.space>
2021-05-25 23:32:21 +0200

    modal on movie not found

--- src/components/views/SearchMovieToAssociateView.tsx
+++ src/components/views/SearchMovieToAssociateView.tsx
@@ -1,27 +1,70 @@
 import { ComponentProps } from "../routeTypings";
 import { MovieSearch } from "../../lib/components/MovieSearch";
-import { CinePinButton } from "../../lib/components/CinePinButton";
-import { View, Text } from "react-native";
+import { Modal, StyleSheet, Text, View } from "react-native";
 import React, { useState } from "react";
 import { Movie } from "../../lib/DataLayer";
+import { CinePinButton } from "../../lib/components/CinePinButton";
 
-export function SearchMovieToAssociateView({route, navigation}: ComponentProps<"Cerca film">){
-  const {pin}= route.params
-  const [movie, setMovie] = useState<Movie>({} as any);
-  const [err, setErr] = useState("");
-  return <View style={{ flex: 1 , marginTop: "30%", margin: 30}}>
-    <MovieSearch onMovieFound={(e, item) => {
-      if (e) {
-          setErr(e);
-          setMovie(null as any)
-      }
-      else if (item) {
-        setMovie(item);
-      }
-    }} onMovieClick={movie=>{
-      if (movie) setMovie(movie)
-      navigation.navigate("Dettagli Scena", {pin, movie})
-    }}/>
-    {err ? <Text> {err} </Text>: null}
-  </View>
+const style = StyleSheet.create({
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: "center",
+        fontSize: 18
+    }
+});
+
+export function SearchMovieToAssociateView({ route, navigation }: ComponentProps<"Cerca film">) {
+    const { pin } = route.params;
+    const [movie, setMovie] = useState<Movie>({} as any);
+    const [err, setErr] = useState("");
+    return (
+        <View style={{ flex: 1, marginTop: "30%", margin: 30 }}>
+            <MovieSearch
+                onMovieFound={(e, _movie) => {
+                    if (e) {
+                        setErr(e);
+                        setMovie(null as any);
+                    } else if (_movie) {
+                        setErr(null as any);
+                        setMovie(_movie);
+                    }
+                }}
+                onMovieClick={movie => {
+                    if (movie) setMovie(movie);
+                    navigation.navigate("Dettagli Scena", { pin, movie });
+                }}
+            />
+            <Modal visible={!!err} transparent={true} animationType={"fade"}>
+                <View style={style.centeredView}>
+                    <View style={style.modalView}>
+                        <Text style={style.modalText}>
+                          {err}
+                        </Text>
+                        <CinePinButton message={"chiudi"} onPress={() => setErr("")} />
+                    </View>
+                </View>
+            </Modal>
+        </View>
+    );
 }
