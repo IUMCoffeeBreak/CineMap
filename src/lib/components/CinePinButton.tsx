f2ec8dd755bfd3df351d4fc013a4808a4b55ffec
dadna <dd@no.space>
2021-06-15 13:00:06 +0200

    icon buttons

--- src/lib/components/CinePinButton.tsx
+++ src/lib/components/CinePinButton.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Button } from "react-native-paper";
 import { StyleProp } from "react-native";
 import constants from "../utils/constants";
+import { IconSource } from "react-native-paper/lib/typescript/components/Icon";
 
 export function CinePinButton<T = any>(props: {
     disabled?: boolean;
     message: string;
     style?: StyleProp<T>;
     onPress?: () => void;
+    icon?: IconSource;
 }) {
     return (
         <Button
@@ -16,6 +18,7 @@ export function CinePinButton<T = any>(props: {
             style={{ ...(props.style as any) }}
             onPress={props?.onPress}
             disabled={props.disabled}
+            icon={props.icon}
         >
             {props.message}
         </Button>
