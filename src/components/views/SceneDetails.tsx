d6cf0a1c3cd7815be40f0a94466fda14d30b4615
gbrlfrc <g.fraracci@redev.technology>
2021-06-17 12:53:43 +0200

    new CreateScene View

--- src/components/views/SceneDetails.tsx
+++ src/components/views/SceneDetails.tsx
@@ -1,5 +1,5 @@
 import { ComponentProps } from "../routeTypings";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import { TextInput } from "react-native-paper";
 import constants from "../../lib/utils/constants";
 import { CinePinButton } from "../../lib/components/CinePinButton";
@@ -7,30 +7,64 @@ import { db } from "../../db";
 import _ from "lodash";
 import React, { useState } from "react";
 import { SafeAreaView } from "../../lib/components/SafeAreaView";
+import { MovieSearch } from "../../lib/components/MovieSearch";
+import { Movie } from "../../lib/DataLayer";
 
-const styles = StyleSheet.create({
-    title: {
-        fontSize: 20,
-        fontWeight: "bold"
-    },
-    input: {
-        height: 45,
-        backgroundColor: "white",
-        marginBottom: 20
-    },
-    submitButton: {
-        marginTop: 1,
-        marginBottom: "10%"
-    }
-});
 
 export function SceneDetails({ route, navigation }: ComponentProps<"Dettagli Scena">) {
     const { pin, movie } = route.params;
     const [sceneTitle, setSceneTitle] = useState("");
     const [sceneLink, setSceneLink] = useState("");
+    const [film, setMovie] = useState<Movie>({} as any);
+    const [err, setErr] = useState("");
+    const [searchModal, setSearchModal] = useState(true);
+
     return (
-        <SafeAreaView style={{ padding: constants.spacing.MARGIN_LEFT }}>
-            <View style={{ padding: 20 }}>
+        <SafeAreaView style={styles.mainContainer}>
+            <View style={styles.locationContainer}>
+                <Text style={styles.text}>
+                    Dove si è svolta la scena che vuoi inserire?
+                </Text>
+                <View style={styles.locationLabel}>
+                    {/* <Image 
+                        style={styles.pinIcon}
+                        source={require('./../../assets/')}
+                    /> */}
+                    <Text style={styles.text}>
+                        Location
+                    </Text>
+                </View>
+            </View>
+            {/* <View style={styles.filmContainer}>
+                <Text style={styles.text}>
+                    Da quale film è tratta la scena che vuoi inserire?
+                </Text>
+                <MovieSearch
+                    onMovieFound={(e, item) => {
+                        if (e) setErr(e);
+                        else if (item) {
+                            setMovie(item);
+                        }
+                    }}
+                    onMovieClick={movie => {
+                        if (movie) setMovie(movie);
+                        setSearchModal(false);
+                    }}
+                />
+                <CinePinButton 
+                    message={''}
+                    onPress={()=> {}}
+                />
+            </View> */}
+            {/* <View style={styles.sceneTitleContainer}>
+
+            </View>
+            <View style={styles.sceneLinkContainer}>
+
+            </View> */}
+
+
+            {/* <View style={{ padding: 20 }}>
                 <Text style={styles.title}>Fornisci il titolo della scena e il link al video YouTube</Text>
                 <TextInput
                     theme={{ colors: { primary: constants.colors.MAIN_GREEN } }}
@@ -67,7 +101,44 @@ export function SceneDetails({ route, navigation }: ComponentProps<"Dettagli Sce
                     message={"Conferma"}
                     disabled={!sceneTitle || !sceneLink || _.isEmpty(movie)}
                 />
-            </View>
+            </View> */}
         </SafeAreaView>
     );
 }
+
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        margin: '0%',
+        padding: '10%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+    },
+    locationContainer: {
+        display: 'flex',
+        flex: 1,
+        justifyContent: 'center',
+
+    },
+    locationLabel: {
+        backgroundColor: 'yellow',
+        width: '100%',
+        justifyContent: 'space-between',
+    },
+    filmContainer: {
+        flex: 1
+    },
+    sceneTitleContainer: {
+        flex: 1
+    },
+    sceneLinkContainer: {
+        flex: 1
+    },
+    text: {
+        alignSelf: 'center',
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    pinIcon: {}
+});
